0b6bc3b37dc173b1a41281932e1b2844012a16b0
ec <varikvi@yahoo.com>
2015-06-11 16:32:33 +0200

    Added preliminary development settings -- see training for more

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var config = require("./config/config.js");
+var ConnectMongo = ("mongodb"); // no connection to external yet...
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+    // dev specific settings
+    app.use(session({secret:config.sessionSecret}))
+}else{
+    // prod specific settings
+    app.use(session({secret:'catscanfly'}))
+}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
